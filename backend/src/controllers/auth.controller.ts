0e40763381733ac262848fbba258a2395f068c21
adarsh946 <adarsh.shukla2724@gmail.com>
2024-12-26 19:29:26 +0530

    updated signupController and zod schema

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,28 @@
-export const signUpController = (req: any, res: any) => {
-  res.send("hii");
+import bcrypt from "bcryptjs";
+import { signUpType } from "../types";
+
+export const signUpController = async (req: any, res: any) => {
+  const parsedData = signUpType.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(403).json({
+      message: "Invalid Input",
+    });
+    return;
+  }
+
+  if (!(parsedData.data.password === parsedData.data.confirmPassword)) {
+    res.status(403).json({
+      message: "Password is wrong",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
+
+  try {
+
+    const user = await 
+
+  } catch (error) {}
 };
 
 export const signInController = (req: any, res: any) => {
---
+++ backend/src/types/index.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const signUpType = z.object({
+  fullname: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
+
+export const signInType = z.object({
+  email: z.string(),
+  password: z.string(),
+});
